d8930bc1c250acf8598f30617620264daf0d5841
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-05-11 21:22:24 +0400

    Support Render Postgres URL format

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 connect_args = {}
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
